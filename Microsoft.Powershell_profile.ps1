ee357a61d452ce607e73edb0c621fa351731bcd5
touka-aoi <toukaseisei@gmail.com>
2024-12-14 19:06:55 +0900

    powershellの設定追加

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,41 +1,91 @@
-Invoke-Expression (&starship init powershell)
+# oh-my-posh config
+oh-my-posh init pwsh --config  'C:\Users\ekule\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.json' | Invoke-Expression
 
-function fzf-cd-src {
-   $selected_dir = ghq list -p | fzf --reverse
-
-   if ($selected_dir) {
-    Set-Location $selected_dir
-    Clear-Host
-    [Microsoft.PowerShell.PSConsoleReadLine]::ClearScreen();
-   }
+function ghq-fzf {
+  $repo = $(ghq list -full-path | fzf --reverse )
+  if (-not [string]::IsNullOrEmpty($repo)) {
+    cd $repo
+  }
 }
 
-function fzf-history {
-   $selected = cat (Get-PSReadlineOption).HistorySavePath | fzf --reverse --tac
-
-   if ($selected) {
-      [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selected)
-   }
-
+Set-PSReadLineKeyHandler -Chord Ctrl+k -ScriptBlock { 
+  ghq-fzf 
+  [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine() 
 }
 
-function fzf-gh {
-   $selected = gh repo list --json sshUrl | ConvertFrom-Json | ForEach-Object { "$($_.sshUrl)" } | fzf --reverse
-
-   if ($selected) {
-      [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selected)
-   }
+function history-fzf {
+  $history=Get-Content (Get-PSReadlineOption).HistorySavePath 
+  $history_reverse=$history[($history.Length-1)..0]
+  $selection = $history_reverse | fzf --reverse --no-sort
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert($selection)
 }
+
+Set-PSReadLineKeyHandler -Chord Ctrl+h -ScriptBlock { history-fzf }
+
+function get-git-repo {
+  $repo=gh repo list --json sshUrl,nameWithOwner --jq '.[] | "\(.sshUrl)"' | fzf --reverse
+  [Microsoft.PowerShell.PSConsoleReadLine]::Insert($repo)
+}
+
+Set-PSReadLineKeyHandler -Chord Ctrl+g -ScriptBlock { get-git-repo }
+
+
+Clear-Host
 
 function gst {
 	git status $args
 }
 
-Set-PSReadLineKeyHandler -Key Ctrl+k -ScriptBlock { fzf-cd-src }
-Set-PSReadLineKeyHandler -Key Ctrl+h -ScriptBlock { fzf-history }
-Set-PSReadLineKeyHandler -Key Ctrl+g -ScriptBlock { fzf-gh }
+function ga {
+	git add $args
+}
 
-Set-Alias x explorer
+function gaa {
+	git add --all $args
+}
 
-$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
+function gcmsg {
+	git commit -vm $args
+}
+
+
+function gb {
+	git branch $args
+}
+
+function x {
+	explorer.exe .
+}
+
+function tf {
+	terraform
+}
+
+function tfa {
+	terraform apply
+}
+
+function tfp {
+	terraform plan
+}
+
+function tfd {
+	terraform destroy
+}
+
+function tfi {
+	terraform init
+}
+
+function tff {
+	terraform fmt
+}
+
+function tfv {
+	terraform validate
+}
+
+set-alias vi 'C:\Program Files\Vim\vim91\vim.exe'
+set-alias vim 'C:\Program Files\Vim\vim91\vim.exe'
+
+if ( Test-Path '~/.inshellisense/pwsh/init.ps1' -PathType Leaf ) { . ~/.inshellisense/pwsh/init.ps1 } 
